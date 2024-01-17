72bb5aeaeda66103a3b2b5e107ea6d8e91e6d577
William Li <52115161+Trollermaner@users.noreply.github.com>
2024-01-16 21:32:13 -0500

    added dawson

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-const express = require("express")
-const app = express()
-const courseRouter = require("./routes/course")
-const cors = require('cors')
+const express = require("express");
+const app = express();
+const courseRouter = require("./routes/course");
+const cors = require("cors");
 
-
-app.use(cors({
-    origin: ["https://www.jacschedule.com"]
-}))
+app.use(
+  cors({
+    origin: ["https://www.jacschedule.com"],
+  }),
+);
 
 //REST API!!!
 
-app.use("/course", courseRouter)
+app.use("/course", courseRouter);
 
-
-app.listen(8080)
+app.listen(8080);
 
 module.exports = app;
